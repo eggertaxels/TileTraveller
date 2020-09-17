e1711481cce8174f6ad64f7f47863c8642ba1c32
Eggert <eggertgeir@gmail.com>
2020-09-17 11:24:27 +0000

    again added so program repeats itself if invalid

--- TileTraveller.py
+++ TileTraveller.py
@@ -8,6 +8,7 @@ col, row = 1, 1
 move_is_right = ('')
 direction = ''
 victory = False
+again = 0
 
 def move(col, row, direction):
     ''' Take in current position and return a new position'''
@@ -22,31 +23,38 @@ def move(col, row, direction):
         col -= 1
     return col, row
 
-def play(victory, direction, move_is_right, col, row):
+def play(victory, direction, move_is_right, col, row, again):
     ''' Main game '''
-    while not victory:  # main loop
-        if direction in move_is_right:
+    while not victory:
+        if direction in move_is_right or again == 1:
             if ((col == 1) and (row == 1)) or ((col == 2) and (row == 1)):  # (1,1) / (2,1)
                 print(f"You can travel: {N}.")
                 move_is_right = ('n')
+                again = 0
             elif (col == 1) and (row == 2):
                 print(f"You can travel: {N} or {E} or {S}.")
                 move_is_right = ('n', 'e', 's')
+                again = 0
             elif (col == 1) and (row == 3):
                 print(f"You can travel: {E} or {S}.")
                 move_is_right = ('e', 's')
+                again = 0
             elif (col == 2) and (row == 2):
                 print(f"You can travel: {S} or {W}.")
                 move_is_right = ('s', 'w')
+                again = 0
             elif (col == 3) and (row == 3):
                 print(f"You can travel: {S} or {W}.")
                 move_is_right = ('s', 'w')
+                again = 0
             elif (col == 2) and (row == 3):
                 print(f"You can travel: {E} or {W}.")
                 move_is_right = ('e', 'w')
+                again = 0
             elif (col == 3) and (row == 2):
                 print(f"You can travel: {N} or {S}.")
                 move_is_right = ('n', 's')
+                again = 0
             elif col == 3 and row == 1:
                 print('Victory!')
                 break
@@ -57,6 +65,7 @@ def play(victory, direction, move_is_right, col, row):
             col, row = move(col, row, direction)
         else:
             print('Not a valid direction!')
+            again = 1
 
 
-play(victory, direction, move_is_right, col, row)
+play(victory, direction, move_is_right, col, row, again)

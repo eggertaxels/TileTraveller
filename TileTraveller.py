609979f5bf8c3e87da8e607bc22bde6759dfe650
Eggert <eggertgeir@gmail.com>
2020-09-17 11:23:14 +0000

    most code is ready

--- TileTraveller.py
+++ TileTraveller.py
@@ -1,1 +1,62 @@
-#test
+# Constants
+N = '(N)orth'
+S = '(S)outh'
+E = '(E)ast'
+W = '(W)est'
+
+col, row = 1, 1
+move_is_right = ('')
+direction = ''
+victory = False
+
+def move(col, row, direction):
+    ''' Take in current position and return a new position'''
+
+    if direction.lower() == 'n':
+        row += 1
+    elif direction.lower() == 's':
+        row -= 1
+    elif direction.lower() == 'e':
+        col += 1
+    elif direction.lower() == 'w':
+        col -= 1
+    return col, row
+
+def play(victory, direction, move_is_right, col, row):
+    ''' Main game '''
+    while not victory:  # main loop
+        if direction in move_is_right:
+            if ((col == 1) and (row == 1)) or ((col == 2) and (row == 1)):  # (1,1) / (2,1)
+                print(f"You can travel: {N}.")
+                move_is_right = ('n')
+            elif (col == 1) and (row == 2):
+                print(f"You can travel: {N} or {E} or {S}.")
+                move_is_right = ('n', 'e', 's')
+            elif (col == 1) and (row == 3):
+                print(f"You can travel: {E} or {S}.")
+                move_is_right = ('e', 's')
+            elif (col == 2) and (row == 2):
+                print(f"You can travel: {S} or {W}.")
+                move_is_right = ('s', 'w')
+            elif (col == 3) and (row == 3):
+                print(f"You can travel: {S} or {W}.")
+                move_is_right = ('s', 'w')
+            elif (col == 2) and (row == 3):
+                print(f"You can travel: {E} or {W}.")
+                move_is_right = ('e', 'w')
+            elif (col == 3) and (row == 2):
+                print(f"You can travel: {N} or {S}.")
+                move_is_right = ('n', 's')
+            elif col == 3 and row == 1:
+                print('Victory!')
+                break
+
+        direction = str(input("Direction: "))
+        direction = direction.lower()
+        if direction in move_is_right:
+            col, row = move(col, row, direction)
+        else:
+            print('Not a valid direction!')
+
+
+play(victory, direction, move_is_right, col, row)
